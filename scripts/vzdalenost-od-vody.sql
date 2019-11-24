4bfae3928e0a8bfc1f8daf4a3d952177b76d7fa1
Jan Michálek <godzilalalala@gmail.com>
2019-11-24 12:23:56 +0100

    s/DESC/ASC/g

--- scripts/vzdalenost-od-vody.sql
+++ scripts/vzdalenost-od-vody.sql
@@ -17,7 +17,7 @@ LEFT JOIN LATERAL (
    ST_Distance(vzorek_lsd.geom_spec_jtsk, "A01_Vodni_tok_CEVT".geom) vzd_1
    FROM podklady."A01_Vodni_tok_CEVT"
    WHERE ST_DWithin(vzorek_lsd.geom_spec_jtsk, "A01_Vodni_tok_CEVT".geom, 5000)
-   ORDER BY ST_Distance(geom_spec_jtsk, "A01_Vodni_tok_CEVT".geom) DESC LIMIT 1
+   ORDER BY ST_Distance(geom_spec_jtsk, "A01_Vodni_tok_CEVT".geom) ASC LIMIT 1
 ) nejblizsireka ON True
 WHERE id_species IN (
    SELECT id_species
